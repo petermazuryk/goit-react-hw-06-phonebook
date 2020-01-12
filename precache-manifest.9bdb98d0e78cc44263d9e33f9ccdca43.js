9f7fc1e58b2a556769afd7c38e682c9e1bff60bd
Peter Mazuryk <petermazuryk1984@gmail.com>
2020-01-12 15:50:26 +0200

    Updates

---
+++ precache-manifest.9bdb98d0e78cc44263d9e33f9ccdca43.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c917a4ef454ab730d87a98f5ccf903",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "280ebeb92fafd3a0523e",
+    "url": "/goit-react-hw-06-phonebook/static/css/2.66c32828.chunk.css"
+  },
+  {
+    "revision": "94b38b071aff32e8edd2",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.721afaf1.chunk.css"
+  },
+  {
+    "revision": "280ebeb92fafd3a0523e",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.8f19f867.chunk.js"
+  },
+  {
+    "revision": "b4439fda37f84e74fdbc66e669b783a5",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.8f19f867.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94b38b071aff32e8edd2",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.895d3a85.chunk.js"
+  },
+  {
+    "revision": "8ef94a824e6f3ee6d7e7",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b34c3905.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/goit-react-hw-06-phonebook/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/goit-react-hw-06-phonebook/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/goit-react-hw-06-phonebook/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/goit-react-hw-06-phonebook/static/media/notification.c0d3c94c.eot"
+  }
+]);
